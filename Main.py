f270e8e936b6cc029ba99511dbc9adcfb6410839
breeeeezy <shreyasmamdyal@gmail.com>
2022-07-14 18:20:21 +1000

    Second commit

--- Main.py
+++ Main.py
@@ -23,13 +23,11 @@ for i in expenses:
     expenses.update({i:inpval})
     income -= inpval
 
-print(expenses)
-print(income)
 
-print("; Expenses ; Weekly Amount ;")
+print ("{:<10} {:<10}".format('Expense', 'Weekly Cost'))
 
-for item in expenses:
-    print(";", item[0],";")
+for key in expenses:
+    print (key, '    :    ', expenses[key])
           
 
 
@@ -37,3 +35,8 @@ for item in expenses:
 # Make a for loop that lets the user update all the dictionary keys' values
 # ask user for weekly income
 # print table showing the total spent and saved (look at mirror for table)
+# table has been aded but need to add how much spent and saved per week below
+# add error message for expense if not valid
+# extra feature:
+# - let user add more categories
+# - allow for specifics within each category
